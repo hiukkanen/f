b3477c44914c27a99c3bf45cfb7ef6dcadad2eaa
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-07-21 22:23:29 +0300

    Use bundle exec

--- fluxflex_deploy.sh
+++ fluxflex_deploy.sh
@@ -4,4 +4,4 @@ export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.
 
 bundle install
 
-rake db:migrate RAILS_ENV=production
+bundle exec rake db:migrate RAILS_ENV=production
